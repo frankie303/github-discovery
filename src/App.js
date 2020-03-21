37753ad9d79c40d21fc07980c2d2ae8c0890df19
Mert <uzeyirmertciflikli@gmail.com>
2020-03-21 16:13:17 +0100

    typo correction

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
       `https://api.github.com/search/users?q=${text}client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ user: res.data.items, loading: false });
+    this.setState({ users: res.data.items, loading: false });
   };
 
   render() {
