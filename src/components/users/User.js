1cbd4d732c36f6456cabba2728024980a552e634
Mert Ciflikli <uzeyirmertciflikli@gmail.com>
2020-05-10 17:14:58 +0200

    hireable or not feature added to user more page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
 class User extends Component {
   componentDidMount() {
-    console.log(1234)
     this.props.getUser(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
   }
 
   render() {
     const {name,
-          avatar_url,
-          location, 
-          bio, 
-          blog, 
-          login, 
-          html_url, 
-          followers, 
-          public_repos, 
-          public_gists, 
-          hireable} = this.props.user;
+      avatar_url,
+      location, 
+      bio, 
+      blog, 
+      login, 
+      html_url, 
+      followers, 
+      public_repos, 
+      public_gists, 
+      hireable} = this.props.user;
 
     const { loading } = this.props;
+
+    if (loading) return <Spinner />;
+
     return (
-      <div>
-        {name}
-      </div>
+      <>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        Hireable: {''}
+        {hireable ? (
+          <i className="fa fa-check text-success" />) : ( 
+          <i className="fas fa-times-circle text-danger" /> 
+        )}
+      </>
     )
   }
 }
