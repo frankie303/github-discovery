e455d898e37085e4a1bd1819c837db723eb51d64
Mert Ciflikli <uzeyirmertciflikli@gmail.com>
2020-05-10 16:40:32 +0200

    now user component gets data from github api

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 
 class User extends Component {
+  componentDidMount() {
+    console.log(1234)
+    this.props.getUser(this.props.match.params.login);
+  }
+
   render() {
+    const {name,
+          avatar_url,
+          location, 
+          bio, 
+          blog, 
+          login, 
+          html_url, 
+          followers, 
+          public_repos, 
+          public_gists, 
+          hireable} = this.props.user;
+
+    const { loading } = this.props;
     return (
       <div>
-        User
+        {name}
       </div>
     )
   }
