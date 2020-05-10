52fdd2595fcee8a7f061b72bc33f20b15b552170
Mert Ciflikli <uzeyirmertciflikli@gmail.com>
2020-05-10 18:46:41 +0200

    repos and repoitem components added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
     users: [],
     loading: false,
     alert: null,
-    user: {}
+    user: {},
+    repos: []
   };
 
   //Search Github users
@@ -39,6 +40,17 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   }
 
+  // Get users repos
+  getUserRepos= async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  }
+
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -50,7 +62,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -78,7 +90,9 @@ class App extends Component {
               <Route exact path='/user/:login' render={props => (
                 <User 
                   {...props } 
-                  getUser={this.getUser} 
+                  getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
+                  repos={repos} 
                   user={user}
                   loading={loading}
                 />
