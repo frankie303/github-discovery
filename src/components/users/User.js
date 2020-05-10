3fece7f25c7bd6f5a9c4e1494dde52ce0f7e83cc
Mert Ciflikli <uzeyirmertciflikli@gmail.com>
2020-05-10 17:54:56 +0200

    added more features to user page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -17,12 +17,14 @@ class User extends Component {
   render() {
     const {name,
       avatar_url,
+      company,
       location, 
       bio, 
       blog, 
       login, 
       html_url, 
-      followers, 
+      followers,
+      following, 
       public_repos, 
       public_gists, 
       hireable} = this.props.user;
@@ -41,6 +43,43 @@ class User extends Component {
           <i className="fa fa-check text-success" />) : ( 
           <i className="fas fa-times-circle text-danger" /> 
         )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img src={avatar_url} className="round-img" alt="" style={{ width: ' 150px'}} />
+            <h1>{name}</h1>
+            <p>{location}</p>
+          </div>
+          <div>
+            {bio && <>
+            <h3>Bio</h3>
+            <p>{bio}</p>
+            </>}
+            <a href={html_url} className="btn btn-dark my-1">Visit Github Profile</a>
+            <ul>
+              <li>
+                {login && <>
+                  <strong>Username: </strong> {login}
+                </>}
+              </li>
+              <li>
+                {company && <>
+                  <strong>Company: </strong> {company}
+                </>}
+              </li>
+              <li>
+                {blog && <>
+                  <strong>Website: </strong> {blog}
+                </>}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
       </>
     )
   }
