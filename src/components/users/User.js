c89dbf773e59de844b101a4560d070a6e4be9798
mert <uzeyirmertciflikli@gmail.com>
2020-11-05 09:52:51 +0100

    responsive

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useContext } from 'react'
-import Spinner from '../layout/Spinner'
-import {Link} from 'react-router-dom'
-import Repos from '../repos/Repos'
+import React, { useEffect, useContext } from 'react';
+import Spinner from '../layout/Spinner';
+import { Link } from 'react-router-dom';
+import Repos from '../repos/Repos';
 import GithubContext from '../../context/github/githubContext';
 
 const User = ({ match }) => {
@@ -15,19 +15,21 @@ const User = ({ match }) => {
     // eslint-disable-next-line
   }, []);
 
-  const {name,
+  const {
+    name,
     avatar_url,
     company,
-    location, 
-    bio, 
-    blog, 
-    login, 
-    html_url, 
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
     followers,
-    following, 
-    public_repos, 
-    public_gists, 
-    hireable} = user;
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user;
 
   if (loading) return <Spinner />;
 
@@ -38,49 +40,65 @@ const User = ({ match }) => {
       </Link>
       Hireable: {''}
       {hireable ? (
-        <i className="fa fa-check text-success" />) : ( 
-        <i className="fas fa-times-circle text-danger" /> 
+        <i className='fa fa-check text-success' />
+      ) : (
+        <i className='fas fa-times-circle text-danger' />
       )}
-      <div className="card grid-2">
-        <div className="all-center">
-          <img src={avatar_url} className="round-img" alt="" style={{ width: ' 150px'}} />
+      <div className='cards card'>
+        <div className='all-center'>
+          <img
+            src={avatar_url}
+            className='round-img'
+            alt=''
+            style={{ width: ' 150px' }}
+          />
           <h1>{name}</h1>
           <p>{location}</p>
         </div>
         <div>
-          {bio && <>
-          <h3>Bio</h3>
-          <p>{bio}</p>
-          </>}
-          <a href={html_url} className="btn btn-dark my-1">Visit Github Profile</a>
+          {bio && (
+            <>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </>
+          )}
+          <a href={html_url} className='btn btn-dark my-1'>
+            Visit Github Profile
+          </a>
           <ul>
             <li>
-              {login && <>
-                <strong>Username: </strong> {login}
-              </>}
+              {login && (
+                <>
+                  <strong>Username: </strong> {login}
+                </>
+              )}
             </li>
             <li>
-              {company && <>
-                <strong>Company: </strong> {company}
-              </>}
+              {company && (
+                <>
+                  <strong>Company: </strong> {company}
+                </>
+              )}
             </li>
             <li>
-              {blog && <>
-                <strong>Website: </strong> {blog}
-              </>}
+              {blog && (
+                <>
+                  <strong>Website: </strong> {blog}
+                </>
+              )}
             </li>
           </ul>
         </div>
       </div>
-      <div className="card text-center">
-        <div className="badge badge-primary">Followers: {followers}</div>
-        <div className="badge badge-success">Following: {following}</div>
-        <div className="badge badge-light">Public Repos: {public_repos}</div>
-        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      <div className='card text-center'>
+        <div className='badge badge-primary'>Followers: {followers}</div>
+        <div className='badge badge-success'>Following: {following}</div>
+        <div className='badge badge-light'>Public Repos: {public_repos}</div>
+        <div className='badge badge-dark'>Public Gists: {public_gists}</div>
       </div>
       <Repos repos={repos} />
     </>
-  )
-}
+  );
+};
 
-export default User
+export default User;
